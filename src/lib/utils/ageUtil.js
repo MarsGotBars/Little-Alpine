cf0b9bb2586bfcce65785fe18b3ae4c538e32f10
MarsGotBars <marcinm@live.nl>
2025-09-11 23:37:41 +0200

    Generating age

---
+++ src/lib/utils/ageUtil.js
@@ -0,0 +1,13 @@
+export function calculateAge(birthdate) {
+  const birth = new Date(birthdate);
+  const today = new Date();
+  let age = today.getFullYear() - birth.getFullYear();
+  const monthDiff = today.getMonth() - birth.getMonth();
+  
+  // Check of verjaardag al is geweest
+  if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birth.getDate())) {
+    age--;
+  }
+  
+  return age;
+}
