389569bc8f4a6b76fa5db36ce23ab05175472240
MarsGotBars <marcinm@live.nl>
2025-09-11 19:44:25 +0200

    Create +layout.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+	import Linescan from '$lib/components/filters/Linescan.svelte';
+	import Frameshadow from '$lib/components/filters/Frameshadow.svelte';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<!-- <Linescan /> -->
+<!-- <Frameshadow /> -->
+
+{@render children?.()}
