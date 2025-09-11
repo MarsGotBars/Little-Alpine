e75c831f09b994562ff30370fc7e79262b9c3288
MarsGotBars <marcinm@live.nl>
2025-09-11 23:38:07 +0200

    creating slug and calc age

--- src/lib/utils/textUtil.js
+++ src/lib/utils/textUtil.js
@@ -1,3 +1,5 @@
+import { calculateAge } from './ageUtil.js';
+
 /**
  * Fix de veldnamen
  * 
@@ -11,6 +13,13 @@ export function normalizeFieldName(fieldName) {
 }
 
 /**
+ * url friendly slug van de title
+ */
+function createSlug(title) {
+  return title.toLowerCase().replace(/\s+/g, '-');
+}
+
+/**
  * Omzetten van de velden en values naar titles en descriptions
  * 
  * Zet mugshot er aan het einde bij als het bestaat
@@ -18,20 +27,30 @@ export function normalizeFieldName(fieldName) {
 export function processPersonData(responseData) {
   const titles = [];
   const descriptions = [];
+  const slugs = [];
   let mugshot = null;
 
   if (responseData) {
     // Eerst willen we alle veldnamen & waarden opslaan (behalve de mugshot)
     Object.entries(responseData).forEach(([fieldName, fieldValue]) => {
       if (fieldName !== "mugshot" && fieldValue !== null && fieldValue !== undefined && typeof fieldValue !== "object") {
-        // We fixen de veldnamen
-        const normalizedTitle = normalizeFieldName(fieldName);
-        titles.push(normalizedTitle);
-        descriptions.push(String(fieldValue));
-      } else if (fieldName === "mugshot" && typeof fieldValue === "object") {
-        // We slaan de mugshot data apart op voor later
-        mugshot = fieldValue;
-      }
+        // Replace birthdate with age
+        if (fieldName === "birthdate") {
+          const age = calculateAge(fieldValue);
+          titles.push("Age");
+          descriptions.push(`${age}`);
+          slugs.push(createSlug("Age"));
+        } else {
+          // We fixen de veldnamen
+          const normalizedTitle = normalizeFieldName(fieldName);
+          titles.push(normalizedTitle);
+          descriptions.push(String(fieldValue));
+          slugs.push(createSlug(normalizedTitle));
+        }
+        } else if (fieldName === "mugshot" && typeof fieldValue === "object") {
+          // We slaan de mugshot data apart op voor later
+          mugshot = fieldValue;
+        }
     });
 
     // We voegen de mugshot toe aan het einde als het bestaat
@@ -40,8 +59,9 @@ export function processPersonData(responseData) {
       titles.push("Media");
       // Placeholder die we aan de voorkant vervangen door de mugshot
       descriptions.push("mugshot");
+      slugs.push(createSlug("Media"));
     }
   }
 
-  return { titles, descriptions, mugshot };
+  return { titles, descriptions, slugs, mugshot };
 }
