773e5f21aefdf5c5e1918fd937fcf237c1b58d6f
MarsGotBars <marcinm@live.nl>
2025-09-12 02:50:57 +0200

    settings added (mostly non-functional)

--- src/lib/components/SettingsForm.svelte
+++ src/lib/components/SettingsForm.svelte
@@ -60,6 +60,21 @@
     }
   });
 </script>
+
+<form action="">
+  <label class="setting-box">
+    <span>Volume</span>
+    <div>
+      <input
+        type="range"
+        name="volume"
+        id="volume"
+        min="0"
+        max="100"
+        value="30"
+      />
+    </div>
+  </label>
   <label class="setting-box">
     <span>UI Color</span>
     <p>{currentColor}</p>
@@ -74,3 +89,83 @@
       />
     </div>
   </label>
+  <label>Autoplay <input type="checkbox" name="" id="" /></label>
+</form>
+
+<style>
+  form {
+    gap: 0.625rem;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    align-self: start;
+  }
+
+  label.setting-box {
+    width: 400px;
+    display: flex;
+    flex-direction: column;
+    border: 5px solid var(--color-text-primary);
+  }
+
+  .setting-box span {
+    font-size: var(--font-size-sm);
+    background: var(--color-text-primary);
+    color: var(--color-background);
+    padding: 0.625rem;
+    width: 100%;
+    text-align: center;
+  }
+
+  .setting-box p {
+    padding: 1.25rem;
+    padding-bottom: 0.625rem;
+  }
+  .setting-box div {
+    padding: 1.25rem;
+    padding-top: 0.625rem;
+  }
+
+  .setting-box input {
+    width: 100%;
+  }
+
+  /* Range input styling */
+  input[type="range"] {
+    -webkit-appearance: none;
+    appearance: none;
+    height: 5px;
+    background: var(--color-text-primary);
+    border-radius: 0;
+    outline: none;
+  }
+
+  /* Webkit browsers (Chrome, Safari, Edge) */
+  input[type="range"]::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 15px;
+    height: 25px;
+    background: var(--color-text-primary);
+    border-radius: 0;
+    cursor: pointer;
+    border: none;
+  }
+
+  /* Firefox */
+  input[type="range"]::-moz-range-thumb {
+    width: 15px;
+    height: 25px;
+    background: var(--color-text-primary);
+    border-radius: 0;
+    cursor: pointer;
+    border: none;
+  }
+
+  input[type="range"]::-moz-range-track {
+    height: 5px;
+    background: var(--color-text-primary);
+    border-radius: 0;
+    border: none;
+  }
+</style>
