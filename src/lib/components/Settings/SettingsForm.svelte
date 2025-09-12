320a4a023780949e56ddb61804629a1166f167be
MarsGotBars <marcinm@live.nl>
2025-09-12 11:55:46 +0200

    form in eigen dir gezet

---
+++ src/lib/components/Settings/SettingsForm.svelte
@@ -0,0 +1,23 @@
+<script>
+  import ColorField from "./Fields/ColorField.svelte";
+  import "./Settings-styles.css";
+  let volume = $state(30);
+</script>
+
+<form action="">
+  <label class="setting-box">
+    <span>Volume</span>
+    <div>
+      <input
+        type="range"
+        name="volume"
+        id="volume"
+        min="0"
+        max="100"
+        value="30"
+      />
+    </div>
+  </label>
+  <ColorField />
+  <label>Autoplay <input type="checkbox" name="" id="" /></label>
+</form>
