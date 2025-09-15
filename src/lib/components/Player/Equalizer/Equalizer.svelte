90904133eb0d0e629e1403d5f2e8083ae4c0609b
MarsGotBars <marcinm@live.nl>
2025-09-15 22:13:03 +0200

    Update Equalizer.svelte

--- src/lib/components/Player/Equalizer/Equalizer.svelte
+++ src/lib/components/Player/Equalizer/Equalizer.svelte
@@ -1,11 +1,18 @@
 <script>
-    let { bars } = $props();
+  let { bars, frequencyData = [] } = $props();
+
+  // Convert elke element in de array naar een percentage
+  function getBarHeight(index) {
+    if (!frequencyData || !frequencyData[index]) return 0;
+    const rawValue = frequencyData[index];
+    const percentage = Math.round((rawValue / 255) * 100);
+
+    return percentage;
+  }
 </script>
 
 <div class="equalizer">
-    {#each {length: bars}, index}
-        <div class="bar" id={index}>
-
-        </div>
-    {/each}
+  {#each { length: bars } as _, index}
+    <div class="bar" id={index} style="--height: {getBarHeight(index)}%;"></div>
+  {/each}
 </div>
