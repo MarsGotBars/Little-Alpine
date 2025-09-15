832ed1ad15088b1785251484df416612d8511d9c
MarsGotBars <marcinm@live.nl>
2025-09-15 22:10:11 +0200

    easy imports

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,15 @@
-// place files you want to import through the `$lib` alias in this folder.
+// List component
+export { default as List } from './components/List/List.svelte';
+
+// Player components
+export { default as MusicPlayer } from './components/Player/MusicPlayer.svelte';
+export { default as TrackInfo } from './components/Player/TrackInfo/TrackInfo.svelte';
+export { default as Equalizer } from './components/Player/Equalizer/Equalizer.svelte';
+
+// Settings components
+export { default as SettingsForm } from './components/Settings/SettingsForm.svelte';
+export { default as ColorField } from './components/Settings/ColorField/ColorField.svelte';
+
+// Filter components
+export { default as Frameshadow } from './components/filters/Frameshadow.svelte';
+export { default as Linescan } from './components/filters/Linescan.svelte';
