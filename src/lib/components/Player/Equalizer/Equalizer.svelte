d0b1bc905d45ee543d5c087019f32695166ba0d8
Marcin Magdziak <marcinm@live.nl>
2025-09-15 16:33:03 +0200

    Create Equalizer.svelte
    #14

---
+++ src/lib/components/Player/Equalizer/Equalizer.svelte
@@ -0,0 +1,11 @@
+<script>
+    let { bars } = $props();
+</script>
+
+<div class="equalizer">
+    {#each {length: bars}, index}
+        <div class="bar" id={index}>
+
+        </div>
+    {/each}
+</div>
