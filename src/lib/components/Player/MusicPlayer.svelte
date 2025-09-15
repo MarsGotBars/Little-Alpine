ce498d33c231fe7bf955cc2901cd2e07a904b021
Marcin Magdziak <marcinm@live.nl>
2025-09-15 16:33:12 +0200

    #14

---
+++ src/lib/components/Player/MusicPlayer.svelte
@@ -0,0 +1,14 @@
+<script>
+  import TrackInfo from "./TrackInfo/TrackInfo.svelte";
+  import Equalizer from "./Equalizer/Equalizer.svelte";
+  import "./MusicPlayer-styles.css";
+  const { tracks } = $props();
+  let customBars = null;
+
+  let bars = $state(customBars ?? 16)
+</script>
+
+<aside class="music-player">
+  <TrackInfo {tracks} {bars} />
+  <Equalizer {bars}/>
+</aside>
