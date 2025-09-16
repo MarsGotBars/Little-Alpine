bfedcec729d1e221a57216b3d400983d21ac17f3
MarsGotBars <marcinm@live.nl>
2025-09-16 11:30:11 +0200

    Update MusicPlayer.svelte
    #14

--- src/lib/components/Player/MusicPlayer.svelte
+++ src/lib/components/Player/MusicPlayer.svelte
@@ -1,13 +1,14 @@
 <script>
   import {TrackInfo, Equalizer} from "$lib";
   import "./MusicPlayer-styles.css";
-  const { tracks, volume } = $props();
+  const { tracks, volume, selectedTrack } = $props();
   let customBars = 32;
+  
   let bars = $state(customBars ?? 16);
   let frequencyData = $state([]);
 </script>
 
 <aside class="music-player">
-  <TrackInfo {tracks} {bars} {volume} bind:frequencyData />
-  <Equalizer {bars} {frequencyData} />
+  <TrackInfo {tracks} {bars} {volume} {selectedTrack} bind:frequencyData />
+  <Equalizer {bars} {frequencyData} {volume} />
 </aside>
